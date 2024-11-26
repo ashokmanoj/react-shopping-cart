8d99b88293deaa81a5f75c1ff31eb27d1874cfe5
manoj <manojc6363530865@gmail.com>
2024-11-26 19:35:26 +0530

    first commit

---
+++ src/context/index.jsx
@@ -0,0 +1,102 @@
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const ShoppingCartContext = createContext(null);
+
+function ShoppingCartProvider({ children }) {
+  const [loading, setLoading] = useState(true);
+  const [listOfProducts, setListOfProducts] = useState([]);
+  const [productDetails, setProductDetails] = useState(null);
+  const [cartItems, setCartItems] = useState([]);
+  const navigate = useNavigate();
+
+  async function fetchListOfProducts() {
+    try {
+      const apiResponse = await fetch("https://dummyjson.com/products");
+      const result = await apiResponse.json();
+
+      if (result && result?.products) {
+        setListOfProducts(result?.products);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error("Failed to fetch products:", error);
+    }
+  }
+
+  function handleAddToCart(getProductDetails) {
+    let cpyExistingCartItems = [...cartItems];
+    const findIndexOfCurrentItem = cpyExistingCartItems.findIndex(
+      (cartItem) => cartItem.id === getProductDetails.id
+    );
+
+    if (findIndexOfCurrentItem === -1) {
+      cpyExistingCartItems.push({
+        ...getProductDetails,
+        quantity: 1,
+        totalPrice: getProductDetails?.price,
+      });
+    } else {
+      cpyExistingCartItems[findIndexOfCurrentItem] = {
+        ...cpyExistingCartItems[findIndexOfCurrentItem],
+        quantity: cpyExistingCartItems[findIndexOfCurrentItem].quantity + 1,
+        totalPrice:
+          (cpyExistingCartItems[findIndexOfCurrentItem].quantity + 1) *
+          cpyExistingCartItems[findIndexOfCurrentItem].price,
+      };
+    }
+
+    setCartItems(cpyExistingCartItems);
+    localStorage.setItem("cartItems", JSON.stringify(cpyExistingCartItems));
+    navigate("/cart");
+  }
+
+  function handleRemoveFromCart(getProductDetails, isFullyRemoveFromCart) {
+    let cpyExistingCartItems = [...cartItems];
+    const findIndexOfCurrentCartItem = cpyExistingCartItems.findIndex(
+      (item) => item.id === getProductDetails.id
+    );
+
+    if (isFullyRemoveFromCart) {
+      cpyExistingCartItems.splice(findIndexOfCurrentCartItem, 1);
+    } else {
+      cpyExistingCartItems[findIndexOfCurrentCartItem] = {
+        ...cpyExistingCartItems[findIndexOfCurrentCartItem],
+        quantity: cpyExistingCartItems[findIndexOfCurrentCartItem].quantity - 1,
+        totalPrice:
+          (cpyExistingCartItems[findIndexOfCurrentCartItem].quantity - 1) *
+          cpyExistingCartItems[findIndexOfCurrentCartItem].price,
+      };
+    }
+
+    setCartItems(cpyExistingCartItems);
+    localStorage.setItem("cartItems", JSON.stringify(cpyExistingCartItems));
+  }
+
+  useEffect(() => {
+    fetchListOfProducts();
+
+    // Safely parse cartItems from localStorage
+    const storedCartItems = localStorage.getItem("cartItems");
+    setCartItems(storedCartItems ? JSON.parse(storedCartItems) : []);
+  }, []);
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        listOfProducts,
+        loading,
+        setLoading,
+        productDetails,
+        setProductDetails,
+        handleAddToCart,
+        cartItems,
+        handleRemoveFromCart,
+      }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+}
+
+export default ShoppingCartProvider;
